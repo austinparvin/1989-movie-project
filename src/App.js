c25f886aefafb9ff71281cd82ef4dcbc732c1333
Austin Parvin <austin.parvin@gmail.com>
2020-03-22 19:21:48 -0400

    Movie component setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react'
 import Movie from './components/Movie'
 
 class App extends Component {
+  state = {
+    movies: [],
+  }
+  componentDidMount() {
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?api_key=37569221602edd820d2cf3580496deaa&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=1989'
+    )
+      .then((response) => {
+        console.log(response)
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data.results)
+        this.setState({ movies: data.results })
+      })
+  }
   render() {
     return (
       <main>
-        <Movie />
+        {this.state.movies.map((movie) => (
+          <Movie
+            movieId={movie.id}
+            movieTitle={movie.original_title}
+            movieOverview={movie.overview}
+            posterPath={movie.poster_path}
+          />
+        ))}
       </main>
     )
   }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react'
 class Movie extends Component {
   render() {
     return (
-      <section className="movie-card">
-        <header className="movie-title">Title</header>
-        <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg" />
-        <p className="plot-description">
-          Lorem blahsdkfv;a efas fea gdaegra evafsa eaf rea ieafa l.
-          fewaerasfewa ewafewaf esafesaf esafesaf esafesa.
-        </p>
+      <section className="movie-card" key={this.props.movieId}>
+        <header className="movie-title">{this.props.movieTitle}</header>
+        <img
+          src={
+            'https://image.tmdb.org/t/p/w185_and_h278_bestv2' +
+            this.props.posterPath
+          }
+        />
+        <p className="plot-description">{this.props.movieOverview}</p>
       </section>
     )
   }
